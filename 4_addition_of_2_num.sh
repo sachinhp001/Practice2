7a323143aac4fdfc12ed5e507608e9b6ba5b2da1
manojugowda <95755255+manojugowda@users.noreply.github.com>
2021-12-10 10:14:35 +0530

    6th line added

--- 4_addition_of_2_num.sh
+++ 4_addition_of_2_num.sh
@@ -3,6 +3,7 @@ echo "***************************************"
 #Testing
 echo "Enter the value of num1"
 read num1
+#6th line added by manoj
 echo "Enter the value of num2"
 read num2
 sum=`expr $num1 + $num2`	
